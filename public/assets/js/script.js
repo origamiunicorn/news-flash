6c8f8019924aed607c8fe48a2d9e4bd62220ebef
origami unicorn <rvwright@gmail.com>
2020-02-23 22:23:12 -0800

    Have modal populating on press of button. Form found inside.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.parallax').parallax();
-    $('.sidenav').sidenav();
     $('.modal').modal();
+    $('.sidenav').sidenav();
     $(document).on("click", "#scrape", scrapeArticles);
     $(document).on("click", "#clear", clearArticles);
     $(document).on("click", "#save", saveArticle);
